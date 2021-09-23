b68b6443de64bef30acdfc17c163c7d0e1916714
Xiangming Xi <xixiangming@126.com>
2021-09-23 15:38:18 +0800

    refactor by extracting dependency graph generating

--- utils/nlp/utils.py
+++ utils/nlp/utils.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from utils.ddparser import DDParser
 from LAC import LAC
 
+from graphviz import Digraph
+
 basedir = Path(__file__).parent.parent.parent  # 项目根目录
 current_dir = Path(__file__).parent  # 当前文件夹
 """
@@ -67,3 +69,44 @@ def get_modifier_as_children_att(word, words, head, dep_rel):
     if modifier == "":
         modifier = None
     return modifier
+
+
+def generate_dep_rel_graph(folder, graph_name, words, relation, head, arguments=None):
+    """
+    生成依存关系图
+    :param folder: str
+    :param graph_name: str
+    :param words: list of str
+    :param relation: list of str
+    :param head: list of str
+    :param arguments: dict
+    :return:
+    """
+
+    if arguments is None:
+        arguments = {}
+    graph_format = arguments.get('graph_format', 'png')
+    root_name = arguments.get('root_name', 'Root')
+    font_name = arguments.get('font_name', "Microsoft YaHei")
+    graph_view = arguments.get('graph_view', False)
+    graph_cleanup = arguments.get('graph_cleanup', True)
+
+    g = Digraph(graph_name, format=graph_format)
+    g.node(name=root_name)
+    for word in words:
+        g.node(name=word, fontname=font_name)
+
+    for i in range(len(words)):
+        if relation[i] not in ['HED']:
+            g.edge(words[i], head[i], label=relation[i], fontname=font_name)
+        else:
+            if head[i] == root_name:
+                g.edge(words[i], root_name, label=relation[i], fontname=font_name)
+            else:
+                g.edge(head[i], root_name, label=relation[i], fontname=font_name)
+
+    try:
+        g.render(graph_name, folder, view=graph_view, cleanup=graph_cleanup)
+    except RuntimeError:
+        g = None
+    return g
